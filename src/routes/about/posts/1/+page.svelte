d2a28b993dc12f9b7fed63e1b887c0b67263fdef
Pavel <korolevpavel2008@gmail.com>
2023-08-23 14:32:34 +0400

    trying to add an image

--- src/routes/about/posts/1/+page.svelte
+++ src/routes/about/posts/1/+page.svelte
@@ -1,9 +1,8 @@
 <div style="display: grid;">
     <article class="main_article">
-        <h2 id="article_title">CSS</h2>
+        <h2 id="article_title">Телефильм "17 мгновений весны"</h2>
         <hr id="hr1">
-        <!-- svelte-ignore a11y-img-redundant-alt -->
-        <img src="#" alt="main-image" id="main_img">
+        <img src="" alt="main">
         <hr id="hr2">
         <p id="article_text">
           <strong>CSS -&nbsp; Cascading Style Sheets «каскадные таблицы стилей».</strong>  формальный язык описания внешнего вида документа (веб-страницы), написанного с использованием языка разметки (чаще всего HTML или XHTML). Также может применяться к любым XML-документам, например, к SVG или XUL.
@@ -26,7 +25,7 @@
 div {
     margin: 0;
     padding: 0;
-}
+} 
 hr {
     height: 2px;
     background-color: black;
@@ -45,22 +44,26 @@ hr {
 
 .main_article {
     position: relative;
-    min-width: 1010px;
-    width: 79vw;
+    /* min-width: 1010px; */
+    /* width: 79vw; */
     background-color: #E0E0E0;
     border-radius: 15px;
-}
+    margin-left: auto;
+    margin-right: auto;
+} 
 #article_title {
     position: absolute;
     top: 0;
-    margin: 0 0 26px 4%;
+    margin-left: auto;
+    margin-right: auto;
     font-weight: 600;
     font-size: 40px;
 }
 #article_text {
     position: relative;
     max-width: 63%;
-    margin-left: 4%;
+    margin-left: auto;
+    margin-right: auto;
     bottom: 120px;
     font-weight: 500;
     font-size: 22px;
@@ -73,10 +76,13 @@ hr {
 #article_title {
     position: static;
     margin: 0 0 60px 0;
+    margin-left: auto;
+    margin-right: auto;
 }
 #article_text {
     position: static;
-    margin: 0 0 40px 0;
+    margin-left: auto;
+    margin-right: auto;
     max-width: unset;
 }
 #hr1 {
