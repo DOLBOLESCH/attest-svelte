0b574f1613b40826256bbe73568735a0fe07bb34
Pavel <korolevpavel2008@gmail.com>
2023-08-24 22:57:48 +0400

    FINAL COMMIT!

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,40 @@
+<svelte:head>
+    <title>Вы можете с нами связаться!</title>
+</svelte:head>
+<div class="container">
+    <h1>Наши контакты</h1>
+    <hr id="hr1">
+        <img src="https://popechitelstvo-zabota.ru/wp-content/uploads/2018/03/contacts-bg1_1-1024x355.jpg" alt="mainpng" class="mainimg">
+    <hr id="hr2">
+        <p>VK.com : @basedstreamsam </p>
+        <p>Telegram : @basedstreamsam</p>
+        <p>Viber : +79967414276</p>
+        <p>Gmail : korolevpavel2008@gmail.com</p>
+        <hr id="final_hr">
+</div>
+
+<style>
+    .mainimg {
+        text-align: center;
+    }
+
+    p {
+        text-decoration: none;
+        display: block;
+        position: relative;
+        font-family: 'Montserrat', sans-serif;
+    }
+
+    hr {
+        height: 2px;
+        background-color: black;
+        border: none;
+    }
+    #final_hr {
+        margin-top: 60px;
+        margin-bottom: 120px;
+        width: 100%;
+    }
+
+</style>
+
